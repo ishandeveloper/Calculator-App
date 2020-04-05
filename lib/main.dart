25adddd2cee5250ec7335488216488ff8f91a4a5
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-05 23:01:31 +0530

    Main Updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,12 @@ import 'homepage.dart';
 
 void main()=>runApp(CalculatorApp());
 
-class CalculatorApp extends StatelessWidget {
+class CalculatorApp extends StatefulWidget {
+  @override
+  _CalculatorAppState createState() => _CalculatorAppState();
+}
+
+class _CalculatorAppState extends State<CalculatorApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
